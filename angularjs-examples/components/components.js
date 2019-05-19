6e24318b5a478da1482b5cf764350e260dcbf5f7
pineoc <kingsuk92@gmail.com>
2019-05-19 17:12:19 +0900

    Added angularjs example code
    - create components
    - 템플릿 파일로 따로 빼서 정리한 내용 추가

---
+++ angularjs-examples/components/components.js
@@ -0,0 +1,48 @@
+/*
+Copyright 2019 Google Inc. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
+
+angular.module('components', [])
+
+  .directive('tabs', function() {
+    return {
+      restrict: 'E',
+      transclude: true,
+      scope: {},
+      controller: function($scope, $element) {
+        var panes = $scope.panes = [];
+
+        $scope.select = function(pane) {
+          angular.forEach(panes, function(pane) {
+            pane.selected = false;
+          });
+          pane.selected = true;
+        };
+
+        this.addPane = function(pane) {
+          if (panes.length == 0) $scope.select(pane);
+          panes.push(pane);
+        };
+      },
+      templateUrl: 'tabs.tpl.html',
+      replace: true
+    };
+  })
+
+  .directive('pane', function() {
+    return {
+      require: '^tabs',
+      restrict: 'E',
+      transclude: true,
+      scope: { title: '@' },
+      link: function(scope, element, attrs, tabsController) {
+        tabsController.addPane(scope);
+      },
+      template:
+        '<div class="tab-pane" ng-class="{active: selected}" ng-transclude>' +
+        '</div>',
+      replace: true
+    };
+  });
